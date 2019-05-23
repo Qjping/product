ddbe9bd4b6fe0b1482afce20154b176e52d28477
Qjping <1490665859@qq.com>
2019-05-23 11:32:09 +0800

    增加更新api

--- src/main/java/qjp/api/controller.java
+++ src/main/java/qjp/api/controller.java
@@ -3,9 +3,7 @@ package qjp.api;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import qjp.domain.Book;
 
 import java.util.List;
@@ -14,13 +12,18 @@ import java.util.List;
 public class controller {
     @Autowired
    SqlSessionTemplate sqlSessionTemplate;
-    @GetMapping("/v1/get/{num}")
-    public Book get(@PathVariable Integer num){
-        return sqlSessionTemplate.selectOne("find",num);
+    @GetMapping("/v1/get/{id}")
+    public Book get(@PathVariable Integer id){
+        return sqlSessionTemplate.selectOne("find",id);
     }
-    @GetMapping("/v1/getall")
+    @GetMapping("/v1/get/all")
     public List<Book> getall(){
         return sqlSessionTemplate.selectList("getAll");
+    }
+    @PostMapping("/v1/update")
+    public Book update(@RequestBody Book book){
+        sqlSessionTemplate.update("update",book);
+        return sqlSessionTemplate.selectOne("find",book.getId());
     }
 }
 
